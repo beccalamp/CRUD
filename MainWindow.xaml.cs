4786432de0f510d7ee67a8ee94079f62ed1c6c62
Rebecca <beccadias9@gmail.com>
2026-05-28 16:55:51 -0300

    remote pagina de cadastro e cria nova pagina inicial

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,63 +1,11 @@
-﻿using System.IO;
 using System.Windows;
-using MySql.Data.MySqlClient;
 
 namespace CRUD;
 
 public partial class MainWindow : Window
 {
-    public string stringConexao = Environment.GetEnvironmentVariable("MYSQL_STRING");
-
-    public MainWindow() 
+    public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void BtnCadastrar_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (string.IsNullOrWhiteSpace(txtNome.Text) || string.IsNullOrWhiteSpace(txtUsername.Text) ||
-            string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtSenha.Password))
-        {
-            MessageBox.Show("Todos os campos são obrigatórios.", "Erro!");
-            return;
-        }
-
-        using (var conexao = new MySqlConnection(stringConexao))
-        {
-            var query = "INSERT INTO usuarios(nome, username, email, senha) VALUES(@nome, @username, @email, @senha)";
-
-            using (var comando = new MySqlCommand(query, conexao))
-            {
-                comando.Parameters.AddWithValue("@nome", txtNome.Text);
-                comando.Parameters.AddWithValue("@username", txtUsername.Text);
-                comando.Parameters.AddWithValue("@email", txtEmail.Text);
-                comando.Parameters.AddWithValue("@senha", txtSenha.Password);
-
-                try
-                {
-                    conexao.Open();
-
-                    var linhasAfetadas = comando.ExecuteNonQuery();
-                    if (linhasAfetadas > 0)
-                    {
-                        MessageBox.Show("Cadastro efetuado com sucesso!");
-                        return;
-                    }
-                }
-                catch (Exception exception)
-                {
-                    if (exception is MySqlException erroSql)
-
-                        if (erroSql.Number == 1062)
-                        {
-                            MessageBox.Show("O email ou usuário já foram utilizados");
-                            return;
-                        }
-
-                    Console.WriteLine(exception);
-                    return;
-                }
-            }
-        }
     }
 }
