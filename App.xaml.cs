dac61a48ab5040d3e07d4448b565f875615bc316
Rebecca <beccadias9@gmail.com>
2026-05-28 16:14:10 -0300

    cria página de cadastro com acesso ao DB

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using DotNetEnv;
+
+namespace CRUD;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Env.Load("C:\\Users\\Aluno\\RiderProjects\\CRUD\\.env");
+        
+        base.OnStartup(e);
+    }
+
+}
