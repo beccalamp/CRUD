657887779cfa741beec1201e1cecaa4be9541fcf
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-06-02 16:56:50 -0300

    envia usuario para MeuPerfil após login
    - Acrescenta a funcionalidade de atualização nos dados do perfil

--- MeuPerfil.xaml.cs
+++ MeuPerfil.xaml.cs
@@ -1,11 +1,81 @@
 ﻿using System.Windows;
+using CRUD.Modelos;
+using MySql.Data.MySqlClient;
 
 namespace CRUD;
 
 public partial class MeuPerfil : Window
 {
-    public MeuPerfil()
+    private Usuario UsuarioAtual;
+
+    public MeuPerfil(Usuario usuario)
     {
         InitializeComponent();
+        UsuarioAtual = usuario;
+        TxtNome.Text = UsuarioAtual.Nome;
+        TxtEmail.Text = UsuarioAtual.Email;
+        TxtUsername.Text = UsuarioAtual.Username;
+    }
+
+    private void BtnSalvar_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(TxtNome.Text))
+        {
+            MessageBox.Show("O campo NOME não pode estar vazio.");
+            TxtNome.Focus();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(TxtEmail.Text))
+        {
+            MessageBox.Show("O campo EMAIL não pode estar vazio.");
+            TxtEmail.Focus();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(TxtUsername.Text))
+        {
+            MessageBox.Show("O campo USERNAME não pode estar vazio.");
+            TxtUsername.Focus();
+            return;
+        }
+
+        var senhaFoiAlterada = !string.IsNullOrWhiteSpace(TxtSenha.Password);
+
+        UsuarioAtual.Username = TxtUsername.Text;
+        UsuarioAtual.Nome = TxtNome.Text;
+        UsuarioAtual.Email = TxtEmail.Text;
+        if (senhaFoiAlterada) UsuarioAtual.Senha = TxtSenha.Password;
+
+        using var conexao = new MySqlConnection(App.StringConexao);
+        var query = "UPDATE usuarios SET username = @username, nome = @nome, email = @email";
+
+        if (senhaFoiAlterada) query += ", senha = @senha";
+
+        query += " WHERE id = @id";
+
+        using var comando = new MySqlCommand(query, conexao);
+
+        comando.Parameters.AddWithValue("@username", UsuarioAtual.Username);
+        comando.Parameters.AddWithValue("@nome", UsuarioAtual.Nome);
+        comando.Parameters.AddWithValue("@email", UsuarioAtual.Email);
+        comando.Parameters.AddWithValue("@id", UsuarioAtual.Id);
+
+        if (senhaFoiAlterada) comando.Parameters.AddWithValue("@senha", UsuarioAtual.Senha);
+        
+        try
+        {
+            conexao.Open();
+            var linhasAfetadas = comando.ExecuteNonQuery();
+
+            if (linhasAfetadas > 0)
+                MessageBox.Show("Cadastro atualizado com sucesso!");
+            else
+                MessageBox.Show("Erro ao atualizar o cadastro!");
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show("Erro de DB.");
+        }
     }
 }
